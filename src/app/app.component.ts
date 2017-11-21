af8b9c044d9c095b6fbc5b79807ae82e759905f5
Jacob-wangquan <510208757@qq.com>
2017-11-21 22:22:51 +0800

    生命周期demo1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Jacob';
+
+  greeting:string = 'hello';
+  user:{name:string} = {name: 'tom'};
+  constructor(){
+
+  }
 }
